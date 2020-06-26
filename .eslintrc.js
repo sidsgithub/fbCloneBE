38c1600ef9ee77826fa27ff6935302be9ee792c1
siddharthp-wal <siddharth.p@westagilelabs.com>
2020-06-26 12:17:34 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended', 'airbnb'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();// initialize an app
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, (error) => {
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`Server started on port ${port}`);
+  }
+});
+
+module.exports = server;
