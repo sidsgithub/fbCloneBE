b70b2a519bc4cd93a8fb93fb28feb7e8d251feb8
siddharthp-wal <siddharth.p@westagilelabs.com>
2020-06-29 21:07:03 +0530

    comment test cases added

--- test/reply.test.js
+++ test/reply.test.js
@@ -3,9 +3,10 @@ const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-describe('POST /addComment', () => {
-  it('should add a comment to a post', () => {});
+describe('POST /reply', () => {
+  it('should add a comment to a comment', () => {});
   it('should throw an error when post does not exist', () => {});
+  it('should throw an error when comment does not exist', () => {});
   it('should throw an error when user is not a friend', () => {});
   it('should throw an error when comment is empty', () => {});
 });
