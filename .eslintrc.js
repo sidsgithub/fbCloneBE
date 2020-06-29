e2830f0d83178c858177a62bb1a9e75e8ee2f8fe
siddharthp-wal <siddharth.p@westagilelabs.com>
2020-06-29 22:54:08 +0530

    eslintsrc changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
+    commonjs: true,
     es2020: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'airbnb'],
+  extends: [
+    'airbnb-base',
+  ],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: 'module',
   },
-  rules: {},
+  rules: {
+  },
 };
