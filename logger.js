890dbdef9b2b7f9e6c1952a18c8e5c680032ff9d
siddharthp-wal <siddharth.p@westagilelabs.com>
2020-07-16 14:18:27 +0530

    logger

---
+++ logger.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'info.log', level: 'info' }),
+    new winston.transports.File({ filename: 'warn.log', level: 'warn' }),
+  ],
+});
+module.exports = logger;
